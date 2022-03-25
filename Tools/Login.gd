ef3c56fa2ec3d885cb9d16a01abcbfa6557008b7
Carlos Sanchez <randomouscrap98@aol.com>
2022-03-25 18:43:22 -0400

    Learning about containers

--- Tools/Login.gd
+++ Tools/Login.gd
@@ -1,16 +1,19 @@
-extends Panel
+extends PanelContainer
 
 signal login_success(url, token)
 
 onready var request = $HTTPRequest
-onready var apiSelect = $VBoxContainer/ApiEndpointSelect
-onready var errorBox =$VBoxContainer/MarginContainer6/ErrorBox
-onready var loginButton = $VBoxContainer/LoginButton
+onready var apiSelect = $MarginContainer/VBoxContainer/ApiEndpointSelect
+onready var errorBox =$MarginContainer/VBoxContainer/MarginContainer6/ErrorBox
+onready var loginButton = $MarginContainer/VBoxContainer/LoginButton
+onready var passwordEdit = $MarginContainer/VBoxContainer/PasswordEdit
+onready var usernameEdit = $MarginContainer/VBoxContainer/UsernameEdit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	request.connect("request_completed", self, "_request_complete")
 	loginButton.connect("pressed", self, "_login_pressed")
+	passwordEdit.connect("text_entered", self, "_password_entered")
 	pass # Replace with function body.
 
 func SetError(text: String):
@@ -33,13 +36,16 @@ func EnableControls():
 	loginButton.disabled = false
 	apiSelect.disabled = false
 	
+func _password_entered(_text: String):
+	_login_pressed()
+	
 func _login_pressed():
 	DisableControls()
 	ClearError()
-	var apiUrl = GetSelectedUrl()
-	print("URL: %s" % apiUrl)
-	var data = { "username": $VBoxContainer/UsernameEdit.text, "password": $VBoxContainer/PasswordEdit.text }
-	var error = request.request(apiUrl + "/user/login", ApiUtilities.GetStandardHeaders(), true,HTTPClient.METHOD_POST, JSON.print(data))
+	var apiUrl = GetSelectedUrl() + "/user/login"
+	print("Login requesting URL: %s" % apiUrl)
+	var data = { "username": usernameEdit.text, "password": passwordEdit.text }
+	var error = request.request(apiUrl, ApiUtilities.GetStandardHeaders(), true,HTTPClient.METHOD_POST, JSON.print(data))
 	if error != OK:
 		SetError("Unknown error from request: %s" % error)
 		EnableControls()
--- Tools/PageChat.gd
+++ Tools/PageChat.gd
@@ -1,17 +1,22 @@
-extends PanelContainer
+extends VBoxContainer
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var toggleExtra = $VBoxContainer/TitlePanel/HBoxContainer/ToggleExtra
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	toggleExtra.connect("toggled", self, "_toggle_extra")
 	pass # Replace with function body.
 
-func SetError(text: String):
-	$ScrollContainer/MarginContainer/ErrorLabel.text = text
+func _toggle_extra():
+	if toggleExtra.is_pressed():
+		$VBoxContainer/ExtraPanel.show()
+	else:
+		$VBoxContainer/ExtraPanel.hide()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
