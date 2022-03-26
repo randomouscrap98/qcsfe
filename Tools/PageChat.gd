1bf5bf605f5bcce88f0be5ec7a3ba80be5568fe5
Carlos Sanchez <randomouscrap98@aol.com>
2022-03-25 22:48:41 -0400

    Actual page format?

--- Tools/PageChat.gd
+++ Tools/PageChat.gd
@@ -4,18 +4,19 @@ extends VBoxContainer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var toggleExtra = $VBoxContainer/TitlePanel/HBoxContainer/ToggleExtra
+onready var toggleExtra = $Header/TitlePanel/HBoxContainer/ToggleExtra #$VBoxContainer/TitlePanel/HBoxContainer/ToggleExtra
+onready var extraPanel = $Header/ExtraPanel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	toggleExtra.connect("toggled", self, "_toggle_extra")
 	pass # Replace with function body.
 
-func _toggle_extra():
+func _toggle_extra(state):
 	if toggleExtra.is_pressed():
-		$VBoxContainer/ExtraPanel.show()
+		extraPanel.show()
 	else:
-		$VBoxContainer/ExtraPanel.hide()
+		extraPanel.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
