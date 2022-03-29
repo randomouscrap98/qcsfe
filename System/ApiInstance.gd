3f59109df641fc122b91c9cb0f17b7912432a0a3
Carlos Sanchez <randomouscrap98@aol.com>
2022-03-28 20:27:31 -0400

    Changing ws design

--- System/ApiInstance.gd
+++ System/ApiInstance.gd
@@ -1,21 +1,41 @@
 extends Node
 
+const RECONNECTINTERVAL = 5.0
+
 var apiUrl : String = ""
+var wsUrl : String = ""
 var userToken : String = ""
 
 var queuedRequests = []
 var currentRequest = null
-var _request
+var request
+var client
+
+var doReconnect : bool = false
+var reconnectTimer = 0
 
 signal login_success()
 signal login_error(message)
 signal all_error(message)
 
+signal ws_connected()
+signal ws_disconnected()
+signal ws_badtoken(message)
+signal ws_unhandled(message)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_request = HTTPRequest.new()
-	add_child(_request)
-	_request.connect("request_completed", self, "_request_completed")
+	request = HTTPRequest.new()
+	client = WebSocketClient.new()
+	add_child(request)
+	request.connect("request_completed", self, "_request_completed")
+	
+	# According to examples, connect signals only once, because you can reuse the
+	# client object forever, even after closing and reopening.
+	client.connect("connection_closed", self, "_ws_closed")
+	# client.connect("connection_error", self, "_ws_error")
+	client.connect("connection_established", self, "_ws_open")
+	client.connect("data_received", self, "_ws_message")
 
 
 # Standard non-state functions
@@ -23,9 +43,35 @@ func GetStandardHeaders():
 	return [ "Content-Type: application/json", "accepts: application/json"]
 
 
+func _ws_closed(_clean = false):
+	printerr("Websocket to %s closed, clean: %d" % [ wsUrl, _clean ])
+	emit_signal("ws_disconnected")
+	# This will reconnect very fast, as fast as possible!
+	if doReconnect:
+		ConnectWs()
+
+func _ws_open(_proto = ""):
+	print("Websocket opened to %s, sending token" % wsUrl)
+
+func _ws_message():
+	pass
+
+func DisconnectWs():
+	if doReconnect:
+		print("Forcing disconnect to ws %s" % wsUrl)
+		doReconnect = false
+		client.disconnect_from_host(1000, "Client forced disconnect?")
+	else:
+		print("Disconnect called, but no websocket seemingly running")
+
+func ConnectWs(timeout = 0):
+	reconnectTimer = timeout
+	
 # Actual stateful functions (ugh)
 func ReInitialize(url : String, loginData):
+	DisconnectWs()
 	apiUrl = url
+	wsUrl = url.replace("http", "ws") + "/live/ws" # This could be dangerous (the replace) but whatever
 	var loginUrl  = url + "/user/login"
 	print("Login (ReInitialize) requesting URL: %s" % loginUrl)
 	QueueRequestPost(loginUrl, GetStandardHeaders(), JSON.print(loginData), "Login")
@@ -62,10 +108,20 @@ func _process(_delta):
 	if queuedRequests.size() > 0 && currentRequest == null:
 		currentRequest = queuedRequests.pop_front()
 		# This DOES NOT block for the entirety of the request!
-		var error = _request.request(currentRequest.url, currentRequest.headers, true, currentRequest.method, currentRequest.data)
+		var error = request.request(currentRequest.url, currentRequest.headers, true, currentRequest.method, currentRequest.data)
 		if error != OK:
 			EmitError(currentRequest.what, "Unknown error from request: %s" % error, currentRequest.error_signals)
 			currentRequest = null
+	if reconnectTimer > 0:
+		reconnectTimer = reconnectTimer - _delta
+		if reconnectTimer <= 0:
+			var err = client.connect_to_url(wsUrl)
+			if err != OK:
+				var errmessage = "COULDN'T INITIALIZE WEBSOCKET TO %s: call returned %d" % [ wsUrl, err ]
+				printerr(errmessage)
+				emit_signal("ws_unhandled", errmessage)
+			else:
+				doReconnect = true
 			
 
 func _request_completed(_result, response_code, _headers, body):
