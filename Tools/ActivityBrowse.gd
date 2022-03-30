eb82aa59d84192a193f85d736217d8d558b573da
Carlos Sanchez <randomouscrap98@aol.com>
2022-03-30 01:42:46 -0400

    Actual hourly parsing

--- Tools/ActivityBrowse.gd
+++ Tools/ActivityBrowse.gd
@@ -16,7 +16,6 @@ func _ready():
 	newButton.connect("pressed", self, "_date_forward")
 	ApiInstance.websocket.connect("ws_request", self, "_request_get")
 	LoadDate()
-	pass # Replace with function body.
 
 
 # This function only looks at the year, month, and day part of the date object. It is assumed
@@ -45,7 +44,12 @@ func _date_forward():
 
 func _request_get(response):
 	if response.id == lastSendId:
-		print("Got response, matched id %s" % lastSendId)
+		print("Got response, matched id %s, error: %s" % [ lastSendId, response.error ])
+		var data = response.data.data
+		# print(JSON.print(data, " "))
+		var result = ApiUtilities.AutoLinkByTimeRequest(data)
+		# print(JSON.print(response.data.search.requests, " "))
+		print(JSON.print(result, " "))
 	else:
 		printerr("Got response that wasn't ours: %s vs %s (ours first)" % [ lastSendId, response.id ])
 
